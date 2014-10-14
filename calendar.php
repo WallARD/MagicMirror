17b08e87fffe278c4a0b849b699a6468e80d1551
root <root@WallNet.(none)>
2014-10-14 14:33:57 +0200

    3rd commit

--- calendar.php
+++ calendar.php
@@ -1,3 +1,3 @@
 <?php
-	$url = 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M';
+	$url = 'https://www.google.com/calendar/ical/pl.polish%23holiday%40group.v.calendar.google.com/public/basic.ics';
 	echo file_get_contents($url);
